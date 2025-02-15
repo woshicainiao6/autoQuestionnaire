9c362381a48e101008e46713633b8ac0b77c1561
LutongZhang <zhanglutong6518@163.com>
2025-02-15 19:47:55 +0800

    优化配置文件

--- static/configuration.py
+++ static/configuration.py
@@ -1,10 +1,22 @@
-
+# API密钥，用于访问外部服务或接口
 api_key = "sk-jrn0VdyFcoBrbUZG92Bd2e35E0A541EeBcDe82F98aCaFc5b"
-base_url = "https://free.gpt.ge/v1/"
+
+# 基础URL，构建请求外部服务或接口的地址
+base_url =  "https://free.v36.cm/v1/"
+
+# 默认的HTTP头部信息，用于所有 outgoing requests
 default_headers = {"x-foo": "true"}
+
+# 代理池URL，用于获取随机代理IP
 proxypool_url = 'http://8.139.255.98:5555/random'
+
+# 目标URL，通过代理访问的最终目的地
 proxy_target_url = 'http://httpbin.org/get'
+
+# 问卷填写URL，指定要自动填写的问卷地址
 # questionAire_url = "https://www.wjx.cn/vm/eo3mdYX.aspx#"
 # questionAire_url='https://www.wjx.cn/vm/wu9f2rZ.aspx'
 questionAire_url="https://www.wjx.cn/vm/rg3dg49.aspx#"
+
+# 问卷填写数量，计划自动填写的问卷数目
 questionAire_num = 500
